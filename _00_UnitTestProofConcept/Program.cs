29514b615d451e89ab6d4cac62436c2983ca5aba
Anthony <adhamlingithub@gmail.com>
2022-04-11 14:11:06 -0500

    concept of UnitTest if/try(Arrange,Act,Assert)/catch

--- _00_UnitTestProofConcept/Program.cs
+++ _00_UnitTestProofConcept/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using _00_UnitTestProofConcept.Tests;
 
+DummyFunctionTest.DummyFunction_ReturnAffirmative_ReturnsString();
 
 Console.WriteLine();  // Space in console output
---
+++ _00_UnitTestProofConcept/Tests/DummyFunctionTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00_UnitTestProofConcept.Tests
+{
+    public class DummyFunctionTest
+    {
+        // be mindful of naming conventions -  ClassName_MethodName_ExpectedResult
+        public static void DummyFunction_ReturnAffirmative_ReturnsString()
+        {
+            try
+            {
+                // Arrange: Get the variables, functions, classes
+                int number = 0;  // for test to pass - must be zero
+                DummyFunction dummyFunction = new DummyFunction();
+
+
+                // Act: Execute 
+                string result = dummyFunction.ReturnAffirmative(number);
+
+                // Assert: is what's return what is expected?
+                if (result == "Affirmative Zero")
+                {
+                    // pass
+                    Console.WriteLine($"PASSED! DummyFunction_ReturnAffirmative_ReturnsString returned {result}");
+                }
+                else
+                {
+                    Console.WriteLine($"FAILED! Expected: 'Affirmative Zero', Actual: {result}");
+                }
+
+
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"Excpetion: {exception}");
+            }
+        }
+    }
+}
