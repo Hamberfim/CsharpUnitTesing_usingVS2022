87be4bd321ec4a1e50aec37567f76a95c3b45412
Anthony <adhamlingithub@gmail.com>
2022-04-12 14:28:37 -0500

    faux network utility tests

---
+++ _01_fauxNetworkUtil.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace _01_fauxNetworkUtil.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ fauxNetworkUtil.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace fauxNetworkUtil.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
