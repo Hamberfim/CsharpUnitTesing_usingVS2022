9a0e48194a7eede8faaba2b7093e26df9c634748
Anthony <adhamlingithub@gmail.com>
2022-04-12 15:28:18 -0500

    tests written and passed

--- _01_fauxNetworkUtil.Tests/PingTests/NetworkServiceTests.cs
+++ _01_fauxNetworkUtil.Tests/PingTests/NetworkServiceTests.cs
@@ -27,5 +27,24 @@ namespace _01_fauxNetworkUtil.Tests.PingTests
             result.Should().Contain("Success", Exactly.Once());
 
         }
+        
+        [Theory]
+        [InlineData(1, 1, 2)]  // int a, int b, int expected
+        [InlineData(2, 2, 4)]  // int a, int b, int expected
+        public void NetworkService_PingTimeOut_ReturnInt(int a, int b, int expected)
+        {
+            // Arrange - get variables, classes, mocks, etc.
+            var pingTimeOutService = new NetworkService();
+
+            // Act - call the method - execute
+            var result = pingTimeOutService.PingTimeOut(a, b);
+
+            // Assert
+            result.Should().Be(expected);  // [InlineData(1, 1, 2)]  // int a, int b, int expected
+            result.Should().BeGreaterThanOrEqualTo(2);  // [InlineData(2, 2, 4)]  // int a, int b, int expected
+            result.Should().NotBeInRange(-1000, 0);  // must be abouve zero
+
+        }
+
     }
 }
